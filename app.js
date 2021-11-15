d847a8e99fe9c4e8006bec77355c942969b21617
tienak1 <thattien1111@gmail.com>
2021-11-16 00:24:34 +0700

    .

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const compression = require('compression');
-const helmet = require('helmet');
+// const compression = require('compression');
+// const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -13,8 +13,8 @@ const productRouter = require('./routes/product');
 
 const app = express();
 
-app.use(helmet());
-app.use(compression()); //Compress all routes
+// app.use(helmet());
+// app.use(compression()); //Compress all routes
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
