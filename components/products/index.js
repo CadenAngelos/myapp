d3b41e935215752ce7ad0ed1a3c748f2c7f025d4
Caden <trinhde530@gmail.com>
2021-11-29 13:36:24 +0700

    CRUD
    week2

--- components/products/index.js
+++ components/products/index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
 const ProductController = require('./productController');
 
 /* GET home page. */
 router.get('/', ProductController.list);
 
-router.get('/:productID', function(req, res, next) {
-    res.render('productDetail', { title: 'Product Detail' });
-  });
+router.get('/:ProductID',ProductController.detail);
   
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// router.get('/:ProductID', function (req, res, next) {
+//   res.send('...................');
+// });
+
 module.exports = router;
